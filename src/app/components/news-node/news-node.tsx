37b11c51becae8eb1a8ee4a72db027cef2d6df77
dkd1998 <dixit.devansh1@gmail.com>
2023-09-08 01:16:52 +0530

    icon additon

--- src/app/components/news-node/news-node.tsx
+++ src/app/components/news-node/news-node.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { AiOutlineCalendar } from "react-icons/ai";
 
 import classes from "./news-node.module.scss";
 
 const NewsNode = (props: any) => {
-  const { imageSource, heading, description } = props;
+  const { imageSource, heading, description, date } = props;
   return (
     <div className={classes["card"]}>
       <img src={imageSource} alt={heading} />
       <h3>{heading}</h3>
+      <div className={classes["card-date"]}>
+        <AiOutlineCalendar size={20} />
+        <span>{date}</span>
+      </div>
       <p>{description}</p>
     </div>
   );
--- src/app/components/news/news.tsx
+++ src/app/components/news/news.tsx
@@ -7,6 +7,7 @@ const data = [
     heading: "The All-New Royal Enfield Classic 350 - Legend Reborn",
     description:
       "Royal Enfield has been a symbol of resilience, purposeful longevity, and authenticity since 1901. It has preserved the legacy of building simple, harmonious classic motorcycles by blending traditional craftsmanship with modern technology, to create pure motorcycling experiences.",
+    date: "23 June 2023",
   },
   {
     imageSource: "/news/2.jpg",
@@ -14,6 +15,7 @@ const data = [
       "The Royal Enfield 650 Twin Motorcycles get new colourways with an array of Make-it-Yours (MiY) Options",
     description:
       "The Royal Enfield 650 Twin Motorcycles get new colourways with an array of Make-it-Yours (MiY) Options",
+    date: "23 June 2023",
   },
   {
     imageSource: "/news/3.jpg",
@@ -21,6 +23,7 @@ const data = [
       "The Royal Enfield 650 Twin Motorcycles get new colourways with an array of Make-it-Yours (MiY) Options",
     description:
       "Interceptor 650 and Continental GT 650 launched in 5 new colours each along with several personalisation options offered via Royal Enfield MiY.",
+    date: "23 June 2023",
   },
   {
     imageSource: "/news/4.jpg",
@@ -28,12 +31,14 @@ const data = [
       "Royal Enfield launches the all-new Easy Cruiser, The Meteor 350    ",
     description:
       "Royal Enfield Meteor 350 cc - Explore specifications, colours, engine, reviews, gallery and more about Meteor.",
+    date: "23 June 2023",
   },
   {
     imageSource: "/news/5.jpg",
     heading: "Royal Enfield launches BS VI compliant Classic 350",
     description:
       "Royal Enfield, the global leader in the mid-size motorcycle segment, announced the launch of the new Bharat Stage VI compliant Royal Enfield Classic 350, the first motorcycle under the company's Unit Construction Engine (UCE) platform to transition to the new regulatory emission norms.",
+    date: "23 June 2023",
   },
   {
     imageSource: "/news/6.jpg",
@@ -41,6 +46,7 @@ const data = [
       "Royal Enfield launches The Classic 500 Tribute Black - A commemorative End of Production Limited Edition motorcycle",
     description:
       "Royal Enfield today announced an end-of-production, special series motorcycles, the Classic 500 Tribute Black Limited Edition.",
+    date: "23 June 2023",
   },
 ];
 const News = () => {
@@ -55,6 +61,7 @@ const News = () => {
             imageSource={item.imageSource}
             description={item.description}
             heading={item.heading}
+            date={item.date}
           ></NewsNode>
         ))}
       </div>
